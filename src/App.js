77d9a5a68118395dc284bfa7fef6880b6ad9b1d9
Boris <anas.essaghir@gmail.com>
2020-05-28 17:17:13 +0200

    Configure Auth0

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './Home';
 import Profile from './Profile';
+import Nav from './Nav';
 
 function App() {
   return (
     <>
-      <Route path="/" component={Home} exact />
-      <Route path="/profile" component={Profile} />
+      <Nav />
+      <div className="body">
+        <Route path="/" component={Home} exact />
+        <Route path="/profile" component={Profile} />
+      </div>
     </>
   );
 }
