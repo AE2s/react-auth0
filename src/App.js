be5a5fb809e6aedc7bc7c055b713ee9f877a5a70
Boris <anas.essaghir@gmail.com>
2020-05-29 18:42:52 +0200

    Some refacto

--- src/App.js
+++ src/App.js
@@ -8,31 +8,35 @@ import Public from './Public';
 import Nav from './Nav';
 import Auth from './Auth/Auth';
 import Callback from './Callback';
+import AuthContext from './AuthContext';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.auth = new Auth(this.props.history);
+    this.state = {
+      auth: new Auth(this.props.history),
+    };
   }
   render() {
+    const { auth } = this.state;
     return (
-      <>
-        <Nav auth={this.auth} />
+      <AuthContext.Provider value={auth}>
+        <Nav />
         <div className="body">
           <Route
             path="/"
             exact
-            render={(props) => <Home auth={this.auth} {...props} />}
+            render={(props) => <Home auth={auth} {...props} />}
           />
           <Route
             path="/callback"
-            render={(props) => <Callback auth={this.auth} {...props} />}
+            render={(props) => <Callback auth={auth} {...props} />}
           />
           <Route
             path="/profile"
             render={(props) =>
-              this.auth.isAuthenticated() ? (
-                <Profile auth={this.auth} {...props} />
+              auth.isAuthenticated() ? (
+                <Profile auth={auth} {...props} />
               ) : (
                 <Redirect to="/" />
               )
@@ -42,26 +46,25 @@ class App extends Component {
           <Route
             path="/private"
             render={(props) =>
-              this.auth.isAuthenticated() ? (
-                <Private auth={this.auth} {...props} />
+              auth.isAuthenticated() ? (
+                <Private auth={auth} {...props} />
               ) : (
-                this.auth.login()
+                auth.login()
               )
             }
           />
           <Route
             path="/courses"
             render={(props) =>
-              this.auth.isAuthenticated() &&
-              this.auth.userHasScopes(['read:courses']) ? (
-                <Courses auth={this.auth} {...props} />
+              auth.isAuthenticated() && auth.userHasScopes(['read:courses']) ? (
+                <Courses auth={auth} {...props} />
               ) : (
-                this.auth.login()
+                auth.login()
               )
             }
           />
         </div>
-      </>
+      </AuthContext.Provider>
     );
   }
 }
--- src/Nav.js
+++ src/Nav.js
@@ -1,38 +1,44 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import AuthContext from './AuthContext';
 
 export default class Nav extends Component {
   render() {
-    const { isAuthenticated, login, logout, userHasScopes } = this.props.auth;
     return (
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-          <li>
-            <Link to="/public">Public</Link>
-          </li>
-          {isAuthenticated() && (
-            <li>
-              <Link to="/private">Private</Link>
-            </li>
-          )}
-          {isAuthenticated() && userHasScopes(['read:courses']) && (
-            <li>
-              <Link to="/courses">Courses</Link>
-            </li>
-          )}
-          <li>
-            <button onClick={isAuthenticated() ? logout : login}>
-              {isAuthenticated() ? 'Log Out' : 'Log In'}
-            </button>
-          </li>
-        </ul>
-      </nav>
+      <AuthContext.Consumer>
+        {(auth) => (
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link to="/public">Public</Link>
+              </li>
+              {auth.isAuthenticated() && (
+                <li>
+                  <Link to="/private">Private</Link>
+                </li>
+              )}
+              {auth.isAuthenticated() && auth.userHasScopes(['read:courses']) && (
+                <li>
+                  <Link to="/courses">Courses</Link>
+                </li>
+              )}
+              <li>
+                <button
+                  onClick={auth.isAuthenticated() ? auth.logout : auth.login}
+                >
+                  {auth.isAuthenticated() ? 'Log Out' : 'Log In'}
+                </button>
+              </li>
+            </ul>
+          </nav>
+        )}
+      </AuthContext.Consumer>
     );
   }
 }
